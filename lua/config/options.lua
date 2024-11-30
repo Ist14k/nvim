1eef255570c4e5cbbbd1463f6a0fcce0f1ad5e2f
Istiak Ahmmed Akash <istiak_akash@outlook.com>
2024-12-01 00:05:04 +0600

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Clipboard settings
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+
+-- Indentation settings
+vim.opt.expandtab = true   -- convert tabs to spaces
+vim.opt.tabstop = 2        -- insert 2 spaces for a tab
+vim.opt.shiftwidth = 2     -- number of spaces for each indentation
+vim.opt.smartindent = true -- autoindent new lines
---
+++ lua/plugins/session/auto-session.lua
@@ -0,0 +1,18 @@
+return {
+    "rmagatti/auto-session",
+    config = function()
+        require("auto-session").setup({
+            auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Projects", "/" },
+            auto_save_enabled = true,
+            auto_session_enable_last_session = true,
+            session_lens = {
+                buffer_to_ignore = {},
+                load_on_start = true,
+                theme_conf = { border = true },
+                previewer = false,
+            },
+            vim.keymap.set("n", "<leader>s", require("auto-session.session-lens").search_session,
+                { noremap = true, desc = "Search session" }),
+        })
+    end
+}
