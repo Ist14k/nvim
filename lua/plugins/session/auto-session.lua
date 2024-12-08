e48f526ca2e2e68d27b06d87c636dc4978cab512
Istiak Ahmmed Akash <istiak_akash@outlook.com>
2024-12-08 16:16:15 +0600

    some empty folder removed

--- lua/plugins/session/auto-session.lua
+++ lua/plugins/session/auto-session.lua
@@ -1,18 +1,23 @@
+-- Description: Auto save and restore sessions
 return {
-    "rmagatti/auto-session",
-    config = function()
-        require("auto-session").setup({
-            auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Projects", "/" },
-            auto_save_enabled = true,
-            auto_session_enable_last_session = true,
-            session_lens = {
-                buffer_to_ignore = {},
-                load_on_start = true,
-                theme_conf = { border = true },
-                previewer = false,
-            },
-            vim.keymap.set("n", "<leader>s", require("auto-session.session-lens").search_session,
-                { noremap = true, desc = "Search session" }),
-        })
-    end
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Projects", "/" },
+      auto_save_enabled = true,
+      auto_session_enable_last_session = true,
+      session_lens = {
+        buffer_to_ignore = {},
+        load_on_start = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+      vim.keymap.set(
+        "n",
+        "<leader>s",
+        require("auto-session.session-lens").search_session,
+        { noremap = true, desc = "Search session" }
+      ),
+    })
+  end,
 }
